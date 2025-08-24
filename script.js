d086d163673b5334ead2166bd0307d34fed981c3
cvm47 <shivamuniyal1810@gmail.com>
2025-08-24 12:44:52 +0530

    feat: add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('illy apturo tapturo')
+function getComputerChoice() {
+    let randomValue = Math.floor(Math.random() * 3)
+    switch (randomValue) {
+        case 0:
+            return 'Love'
+        case 1:
+            return 'Lust'
+        case 2:
+            return 'Logic'
+    }
+}
