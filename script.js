bfbd9421082a0bfed3dc778de7f0415401b858af
cvm47 <shivamuniyal1810@gmail.com>
2025-08-25 23:17:53 +0530

    remove: unnecessary alert()

--- script.js
+++ script.js
@@ -54,8 +54,7 @@ function playGame() {
     }
 
     console.log(`humanScore = ${humanScore} & computerScore = ${computerScore}`)
-
-    alert('Open the console HANDSOME, to see the score \nand remember nothing beats LUST nothing means nothing except :\nCASTRATION or VASECTOMY')
+    
 }
 
 playGame()
